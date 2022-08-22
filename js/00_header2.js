4dd08939f4328954814a85eecf6b5cded9ec8ea8
zakumann <samuel742able@gmail.com>
2022-08-22 22:58:03 +0900

    서브메뉴를 클릭하면 해당 페이지 내에서 이동한 이후에 자동으로 사라지게 했음.

--- js/00_header2.js
+++ js/00_header2.js
@@ -1,10 +1,15 @@
 $(document).ready(function(){
     $('.menubar').click(function(){
-        const nav_menu = document.getElementsByClassName('nav_menu');
+        const nav_menu = document.getElementsByClassName('nav_menu')
         if($(nav_menu).hasClass('close')){
             $(nav_menu).addClass('more').removeClass('close');
         }else if($(nav_menu).hasClass('more')){
-            $(nav_menu).addClass('close').removeClass('more');
+            setTimeout(function() { $(nav_menu).addClass('close').removeClass('more');}, 500);
         }
+    });
+    
+    $('header #nav_gnb nav > ul ul a').click(function() {
+         $('.nav_menu').addClass('close');
+         setTimeout(function() { $('.nav_menu').addClass('close').removeClass('more');}, 500);
     });
 });
