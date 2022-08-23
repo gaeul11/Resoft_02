d4742b0f309b1403d1168371ce076ee00f927fe3
zakumann <samuel742able@gmail.com>
2022-08-23 17:24:50 +0900

    메인페이지의 서브메뉴 부분을 수정

--- js/00_header.js
+++ js/00_header.js
@@ -18,3 +18,7 @@
             $('.stop').hide();
             $('.start').show();
         });
+        $('header #nav_gnb nav > ul ul a').click(function() {
+            $('.nav_menu').addClass('close');
+            setTimeout(function() { $('.nav_menu').addClass('close').removeClass('more');}, 500);
+       });
